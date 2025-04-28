488a190124afd115721d444ef2a3854a63c1ad48
guiwuqi <107486303+guiwuqi@users.noreply.github.com>
2025-04-28 13:40:55 +0800

    Add files via upload

---
+++ transformer/datasets.py
@@ -0,0 +1,122 @@
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+import re
+
+class TranslateDataset(Dataset):
+    def __init__(self, source_tokenizer, target_tokenizer, source_data=None, target_data=None, source_max_seq_len=256, target_max_seq_len=256, phase="train"):
+        self.source_data = source_data
+        self.target_data = target_data
+        self.source_tokenizer = source_tokenizer
+        self.target_tokenizer = target_tokenizer
+        self.source_max_seq_len = source_max_seq_len
+        self.target_max_seq_len = target_max_seq_len
+        self.phase = phase
+    
+    def preprocess_seq(self, seq):
+        seq = re.sub(
+        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(seq))
+        seq = re.sub(r"[ ]+", " ", seq)
+        seq = re.sub(r"\!+", "!", seq)
+        seq = re.sub(r"\,+", ",", seq)
+        seq = re.sub(r"\?+", "?", seq)
+        seq = seq.lower()
+        return seq
+
+    def convert_line_uncased(self, tokenizer, text, max_seq_len):
+        tokens = tokenizer.tokenize(text)[:max_seq_len-2]
+        tokens = [tokenizer.cls_token] + tokens + [tokenizer.sep_token]
+        tokens += [tokenizer.pad_token]*(max_seq_len - len(tokens))
+        token_idx = tokenizer.convert_tokens_to_ids(tokens)
+        return tokens, token_idx
+    
+    def __len__(self):
+        return len(self.source_data)
+
+    # create decoder input mask
+    def create_decoder_mask(self, seq_len):
+        mask = torch.ones(seq_len, seq_len).tril()
+        return mask
+    
+    def __getitem__(self, index):
+        # source_seq, source_idx = self.convert_line_uncased(
+        #     tokenizer=self.source_tokenizer, 
+        #     text=self.preprocess_seq(self.source_data[index]), 
+        #     max_seq_len=self.source_max_seq_len
+        # )
+        # target_seq, target_idx = self.convert_line_uncased(
+        #     tokenizer=self.target_tokenizer, 
+        #     text=self.preprocess_seq(self.target_data[index]), 
+        #     max_seq_len=self.target_max_seq_len
+        # )
+        
+        source = self.source_tokenizer(
+                text=self.preprocess_seq(self.source_data[index]),
+                padding="max_length", 
+                max_length=self.source_max_seq_len, 
+                truncation=True, 
+                return_tensors="pt"
+            )
+        
+        if self.phase == "train":
+            target = self.target_tokenizer(
+                text=self.preprocess_seq(self.target_data[index]),
+                padding="max_length",
+                max_length=self.target_max_seq_len,
+                truncation=True,
+                return_tensors="pt"
+            )
+
+            return {
+                "source_seq": self.source_data[index],
+                "source_ids": source["input_ids"][0],
+                "target_seq": self.target_data[index],
+                "target_ids": target["input_ids"][0],
+                }
+        else:
+            return {
+                "source_seq": self.source_data[index],
+                "source_ids": source["input_ids"][0],
+            }
+
+
+def main():
+    from utils import configs
+
+    def read_data(source_file, target_file):
+        source_data = open(source_file, encoding='gbk', errors='ignore').read().strip().split("\n")
+        target_data = open(target_file, encoding='gbk', errors='ignore').read().strip().split("\n")
+        return source_data, target_data
+
+    train_src_data, train_trg_data = read_data(configs["train_source_data"], configs["train_target_data"])
+    valid_src_data, valid_trg_data = read_data(configs["valid_source_data"], configs["valid_target_data"])
+
+    source_tokenizer = AutoTokenizer.from_pretrained(configs["source_tokenizer"])
+    target_tokenizer = AutoTokenizer.from_pretrained(configs["target_tokenizer"])
+    train_dataset = TranslateDataset(
+        source_tokenizer=source_tokenizer, 
+        target_tokenizer=target_tokenizer, 
+        source_data=train_src_data, 
+        target_data=train_trg_data, 
+        source_max_seq_len=configs["source_max_seq_len"],
+        target_max_seq_len=configs["target_max_seq_len"]
+    )
+    print(train_dataset[0])
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=configs["batch_size"],
+        shuffle=True
+    )
+    for batch in train_loader:
+        print(batch["source_seq"])
+        print(batch["target_seq"])
+        print(batch["source_ids"])
+        print(batch["target_ids"])
+        break
+
+    #import ipdb; ipdb.set_trace()
+
+
+if __name__ == "__main__":
+    main()
